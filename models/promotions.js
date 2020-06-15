e094084afe4f40859e7ad27586ec5f11e2947f70
TomasG <tomasgahan70@gmail.com>
2020-06-15 02:43:58 -0300

    Basic Authentication

--- models/promotions.js
+++ models/promotions.js
@@ -30,6 +30,11 @@ const promotionSchema = new Schema({
     description: {
         type: String,
         required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+
     }
 }, {
     timestamps: true
